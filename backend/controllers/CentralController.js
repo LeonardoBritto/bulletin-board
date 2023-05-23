fa224792889cb0559196881088217ee88e8dede4
Leonardo <leonardobrito992@gmail.com>
2023-05-23 00:24:02 -0300

    ...

--- backend/controllers/CentralController.js
+++ backend/controllers/CentralController.js
@@ -2,6 +2,7 @@ const Central = require('../models/Central')
 
 module.exports = class CentralController {
     static async listarAvisos(req, res){
-        
+       const central = await Central.findAll()
+        res.status(200).json({central: central})   
     }
 }
